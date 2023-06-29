68dd90b35de351681f4e3f4d1e2a01d8f3b57895
Andrey <andreyavs.00@gmail.com>
2023-06-29 16:38:00 +0300

    feat: refactor code

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable(number) {
 
- let arrSimple=['','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
- let arrTen=[' ','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
-let str='';
+    let arrSimple = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let arrTen = [' ', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let str = '';
 
- if(number>=100){
-   if(number%100<20 ) {
-      str= arrSimple[ Math.floor(number/100)]+' hundred '+arrSimple[number%100];
-   }
-   else {
-    str= arrSimple[ Math.floor(number/100)]+' hundred '+ arrTen[ (Math.floor(number/10))%10-1]+ ' '+arrSimple[number%10];
-   } 
- }
- else if(number<100&&number>=20){
-   str= arrTen[Math.floor(number/10)-1]+' '+arrSimple[number%10];
- }
- else if(number===0) {
-   str='zero';
- }
- else {
-   str=arrSimple[number];
- }
+    if (number >= 100) {
+        if (number % 100 < 20) {
+            str = arrSimple[Math.floor(number / 100)] + ' hundred ' + arrSimple[number % 100];
+        }
+        else {
+            str = arrSimple[Math.floor(number / 100)] + ' hundred ' + arrTen[(Math.floor(number / 10)) % 10 - 1] + ' ' + arrSimple[number % 10];
+        }
+    }
+    else if (number < 100 && number >= 20) {
+        str = arrTen[Math.floor(number / 10) - 1] + ' ' + arrSimple[number % 10];
+    }
+    else if (number === 0) {
+        str = 'zero';
+    }
+    else {
+        str = arrSimple[number];
+    }
 
- str=str.trim();
- return str;
- 
+    str = str.trim();
+    return str;
 }
